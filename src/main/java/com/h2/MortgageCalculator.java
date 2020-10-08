3902016d6dc8ec13bd5bc38f65ff97ee4bb5445a
tomasz-paluch <tomasz.m.paluch@gmail.com>
2020-10-08 21:23:42 +0200

    Handling Errors and Bad Inputs. 6 passing

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -15,9 +15,12 @@ public class MortgageCalculator {
     }
 
     public static void main(String[] args) {
-        long loanAmount = Long.parseLong(args[0]);
-        int termInYears = Integer.parseInt(args[1]);
-        float annualRate = Float.parseFloat(args[2]);
+//        long loanAmount = Long.parseLong(args[0]);
+        long loanAmount = Utilities.getLongValue(args[0]);
+//        int termInYears = Integer.parseInt(args[1]);
+        int termInYears = Utilities.getIntValue(args[1]);
+//        float annualRate = Float.parseFloat(args[2]);
+        float annualRate = Utilities.getFloatValue(args[2]);
         MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
         calculator.calculateMonthlyPayment();
         System.out.println(calculator.toString());
